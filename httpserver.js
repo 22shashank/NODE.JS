cdd66252b714715681157afce81a4e16d8b84cf4
shashank <singhshashank15082@gmail.com>
2026-09-15 17:08:08 +0530

    changes in httpserver.change

---
+++ httpserver.js
@@ -0,0 +1,8 @@
+const http('http')
+const port process.env.PORT || 3000;
+const server = htpp.createServer((req,res)=>)
+
+res.statusCode = 200;
+res.setHeader('Content-Type','text/plain');
+res..end('<h1> this is CodeWithHarry</h1><p>hey this is my first nodejs server</p>');
+
